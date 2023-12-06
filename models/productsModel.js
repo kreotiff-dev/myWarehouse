2d283c9c2df3ff24d5034970be4e26caa57d9360
Sychev Eugene <dev.stratus@gmail.com>
2023-12-07 02:16:48 +0300

    Добавлены новые поля в модель продукта

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,15 +1,36 @@
-const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const productSchema = new mongoose.Schema({
-  categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+const productSchema = new Schema({
+  categoryId: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
+  subcategoryId: { type: Schema.Types.ObjectId, ref: 'Subcategory' },
   name: { type: String, required: true },
   price: { type: Number, required: true },
+  currency: { type: String, enum: ['USD', 'EUR', 'GBP', 'RUB'] },
   discountPrice: { type: Number },
   description: { type: String },
   images: [{ type: String }],
   rating: { type: Number },
+  availability: { type: Boolean, default: true },
+  quantity: { type: Number, default: 0 },
+  manufacturer: { type: String },
+  brand: { type: String },
+  weight: { type: Number },
+  weightUnit: { type: String, enum: ['grams', 'kilograms', 'pounds', 'ounces'] },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  tags: [{ type: String }],
+  reviews: [
+    {
+      user: { type: Schema.Types.ObjectId, ref: 'User' },
+      text: { type: String },
+      rating: { type: Number },
+      createdAt: { type: Date, default: Date.now },
+    },
+  ],
+  featured: { type: Boolean, default: false },
+  promotional: { type: Boolean, default: false },
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Product = model('Product', productSchema);
 
 module.exports = Product;
--- routes/v1/products.js
+++ routes/v1/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../../models/productsModel');
+const Product = require('../../models/productsModel').default;
 const multer = require('multer');
 const path = require('path');
 
